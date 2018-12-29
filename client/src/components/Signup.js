98a73c85c3873a8935a0d6dfd4004417f60f8d78
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-12-28 17:49:16 -0800

    Move Signup styles

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,70 +1,10 @@
 import axios from "axios";
 import PropTypes from "prop-types";
 import React, { Component } from "react";
-import styled from "styled-components";
+
+import SignupStyled from "./Signup.styled.js";
 
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:9000";
-
-// Styles
-const SignupStyled = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 20px;
-  width: 100%;
-  height: 100vh;
-  background-color: rgb(243, 243, 243);
-  border-left: 1px solid rgb(151, 151, 151);
-  border-right: 1px solid rgb(151, 151, 151);
-  align-items: center;
-
-  h2 {
-    padding: 20px;
-  }
-
-  h3 {
-    margin: 10px;
-    color: red;
-  }
-
-  button {
-    width: 100px;
-    margin-top: 20px;
-    height: 50px;
-    background-color: rgb(94, 190, 195);
-    color: #ffffff;
-    outline: none;
-    font-size: 0.9rem;
-    font-weight: bold;
-    border: none;
-
-    &:hover {
-      border: 2px solid white;
-    }
-  }
-
-  input {
-    outline: 1px solid rgba(0 0 0 0);
-    border-style: solid;
-    border: 1px solid grey;
-    padding: 15px;
-    margin: 10px;
-    width: 30%;
-    min-width: 200px;
-    font-size: 16px;
-
-    &:hover {
-      border-style: solid;
-      outline: 1px solid rgba(0 0 0 0);
-      border: 1px solid black;
-    }
-
-    &:focus {
-      outline: 1px solid rgb(94, 190, 195);
-      border: 1px solid rgb(94, 190, 195);
-      border-style: solid;
-    }
-  }
-`;
 
 // Signup Component
 class Signup extends Component {
