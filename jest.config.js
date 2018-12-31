e6851993946eabe39a459fa5b48e50c4b6770252
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-12-30 22:40:30 -0800

    Initial setup for mongodb test environment

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/client']
+  testPathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/client'],
+  globalSetup: './tests/setup.js',
+  globalTeardown: './tests/teardown.js',
+  testEnvironment: './tests/mongo-environment.js'
 };
