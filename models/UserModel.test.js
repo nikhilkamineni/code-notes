a2187d502a649d0f72ed44d6cb536506ccdce406
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-12-31 01:08:57 -0800

    Add UserModel test; ignore globalConfig.json

---
+++ models/UserModel.test.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const User = require('./UserModel.js');
+
+describe('insert', () => {
+  let connection;
+  let db;
+  const options = { useNewUrlParser: true, dbName: global.__MONGO_DB_NAME__ }; // fixes deprecation warnings
+
+  beforeAll(async () => {
+    await mongoose.connect(
+      global.__MONGO_URI__,
+      options
+    );
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
+  it('should insert a User into collection', async () => {
+    // const users = db.collection('users');
+
+    const mockUser = await new User({ username: 'testUser1', password: '123456' }).save();
+
+    const savedUser = await User.findOne({ username: 'testUser1' });
+    expect(savedUser.toJSON()).toEqual(mockUser.toJSON());
+  });
+});
