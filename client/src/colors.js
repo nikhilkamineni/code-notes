826581acffe8cee1561dd2064ab8d4e87baac2df
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2019-01-03 14:17:59 -0800

    Add dark theme fields to colors

--- client/src/colors.js
+++ client/src/colors.js
@@ -1,19 +1,64 @@
 const colors = {
-  background1: { light: "#d8d8d8" }, // Sidebar
-  background2: { light: "#f3f3f3" }, // Content
-  background3: { light: "#e1e1e1" }, // Markdown code blocks
-  background4: { light: "#fff" }, // NoteCard, Login/Signup Containers
-  background5: { light: "#eee" }, //NoteDetails content
-  background6: { light: "#f9f9f9" }, // Content
-  background7: { light: "rgba(0, 0, 0, 0.3)" }, // transparent background for modal
-  border1: { light: "#979797" }, // main component borders
-  border2: { light: "#808080" }, // border for input elements
-  border3: { light: "#000" }, // border for input elements
-  highlight1: { light: "#5ebec3" }, // blue used on buttons
-  highlight2: { light: "#bf2828" }, // red used on buttons (e.g. DeleteModal)
-  font1: { light: "#444" }, // Main text
-  font2: { light: "#fff" }, // Buttons
-  font3: { light: "#d72b3f" } // Markdown code font
+  background1: {
+    light: "#d8d8d8",
+    dark: "#d8d8d8"
+  }, // Sidebar
+  background2: {
+    light: "#f3f3f3",
+    dark: "#f3f3f3"
+  }, // Content
+  background3: {
+    light: "#e1e1e1",
+    dark: "#e1e1e1"
+  }, // Markdown code blocks
+  background4: {
+    light: "#fff",
+    dark: "#fff"
+  }, // NoteCard, Login/Signup Containers
+  background5: {
+    light: "#eee",
+    dark: "#eee"
+  }, //NoteDetails content
+  background6: {
+    light: "#f9f9f9",
+    dark: "#f9f9f9"
+  }, // Content
+  background7: {
+    light: "rgba(0, 0, 0, 0.3)",
+    dark: "rgba(0, 0, 0, 0.3)"
+  }, // transparent background for modal
+  border1: {
+    light: "#979797",
+    dark: "#979797"
+  }, // main component borders
+  border2: {
+    light: "#808080",
+    dark: "#808080"
+  }, // border for input elements
+  border3: {
+    light: "#000",
+    dark: "#000"
+  }, // border for input elements
+  highlight1: {
+    light: "#5ebec3",
+    dark: "#5ebec3"
+  }, // blue used on buttons
+  highlight2: {
+    light: "#bf2828",
+    dark: "#bf2828"
+  }, // red used on buttons (e.g. DeleteModal)
+  font1: {
+    light: "#444",
+    dark: "#444"
+  }, // Main text
+  font2: {
+    light: "#fff",
+    dark: "#fff"
+  }, // Buttons
+  font3: {
+    light: "#d72b3f",
+    dark: "#d72b3f"
+  } // Markdown code font
 };
 
 export default colors;
