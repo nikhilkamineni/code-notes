e5687368da649283e6e6d5440415c176084d366e
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2019-01-06 23:32:09 -0800

    Implement basic CM editor in NoteEdit

--- client/src/components/NoteEdit/NoteEdit.js
+++ client/src/components/NoteEdit/NoteEdit.js
@@ -1,38 +1,70 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
+import { UnControlled as CodeMirror } from "react-codemirror2";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/darcula.css";
+import "codemirror/theme/xq-light.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/markdown/markdown";
+import "codemirror/mode/clike/clike";
+import "codemirror/mode/css/css";
+import "codemirror/mode/htmlmixed/htmlmixed";
 
 import NoteEditStyled from "./NoteEdit.styled.js";
+
+const languages = [
+  "markdown",
+  "xml",
+  "javascript",
+  "clike",
+  "css",
+  "htmlmixed"
+];
 
 // NoteEdit Component
 class NoteEdit extends Component {
   state = {
     title: this.props.noteDetails,
     description: this.props.noteDetails.description,
+    language: this.props.noteDetails.language,
     content: this.props.noteDetails.content,
-    _id: this.props.noteDetails._id
+    _id: this.props.noteDetails._id,
+    lineNumbers: true
   };
 
   componentDidMount() {
     this.setState({ ...this.props.noteDetails });
   }
 
-  handleContentInput = e => {
-    this.setState({ content: e.target.value });
+  handleTitleInput = e => {
+    this.setState({ title: e.target.value });
   };
 
   handleDescriptionInput = e => {
     this.setState({ description: e.target.value });
   };
 
-  handleTitleInput = e => {
-    this.setState({ title: e.target.value });
+  handleLanguageDropdown = e => {
+    this.setState({ language: e.target.value });
+  };
+
+  handleContentInput = (editor, data, value) => {
+    this.setState({ content: value });
+  };
+
+  handleLineNumbers = e => {
+    this.setState({ lineNumbers: e.target.checked });
   };
 
   handleUpdate = () => {
-    this.props.updateNote({ ...this.state });
+    const { title, description, language, content, _id } = this.state;
+    this.props.updateNote({ title, description, language, content, _id });
   };
 
   render() {
+    const cmTheme = this.props.theme === "dark" ? "darcula" : "xq-light";
+
     return (
       <NoteEditStyled className="NoteEdit" theme={this.props.theme}>
         <h2>Edit Note:</h2>
@@ -50,15 +82,54 @@ class NoteEdit extends Component {
           value={this.state.description}
           onChange={this.handleDescriptionInput}
         />
-        <textarea
+
+        <div className="NoteEdit__Options">
+          <select
+            className="Options__LanguageDropDown"
+            name="language"
+            onChange={this.handleLanguageDropdown}
+          >
+            {languages.map(lang => (
+              <option
+                value={lang}
+                key={lang}
+                selected={this.state.language === lang}
+              >
+                {lang}
+              </option>
+            ))}
+          </select>
+
+          <div className="Options__LineNumbers">
+            <label>Line Numbers</label>
+            <input
+              defaultChecked={this.state.lineNumbers}
+              type="checkbox"
+              name="lineNumbers"
+              onClick={this.handleLineNumbers}
+            />
+          </div>
+        </div>
+
+        <CodeMirror
           className="NoteEdit__ContentInput"
-          type="text"
-          cols="50"
-          rows="10"
-          placeholder="Content"
           value={this.state.content}
+          options={{
+            mode: this.state.language,
+            theme: cmTheme,
+            lineNumbers: this.state.lineNumbers
+          }}
           onChange={this.handleContentInput}
         />
+        {/* <textarea */}
+        {/*   className="NoteEdit__ContentInput" */}
+        {/*   type="text" */}
+        {/*   cols="50" */}
+        {/*   rows="10" */}
+        {/*   placeholder="Content" */}
+        {/*   value={this.state.content} */}
+        {/*   onChange={this.handleContentInput} */}
+        {/* /> */}
         <button onClick={this.handleUpdate}>Update</button>
       </NoteEditStyled>
     );
@@ -69,6 +140,7 @@ NoteEdit.propTypes = {
   noteDetails: PropTypes.shape({
     title: PropTypes.string,
     description: PropTypes.string,
+    language: PropTypes.string,
     content: PropTypes.string,
     _id: PropTypes.string
   }),
