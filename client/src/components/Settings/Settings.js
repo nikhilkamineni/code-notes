2b4c25c867a0067f2cf0df079cfcf48a7c6a2086
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2019-01-14 23:58:25 -0800

    Convert Settings to functional component

--- client/src/components/Settings/Settings.js
+++ client/src/components/Settings/Settings.js
@@ -1,30 +1,25 @@
 import PropTypes from "prop-types";
-import React, { Component } from "react";
+import React from "react";
 
 import SettingsStyled from "./Settings.styled.js";
 import ChangePassword from "./ChangePassword";
 import ChangeTheme from "./ChangeTheme";
 
-class Settings extends Component {
-  render() {
-    return (
-      <SettingsStyled className="Settings" theme={this.props.theme}>
-        <header>
-          <h2>Settings</h2>
-        </header>
+const Settings = props => {
+  return (
+    <SettingsStyled className="Settings" theme={props.theme}>
+      <header>
+        <h2>Settings</h2>
+      </header>
 
-        <div id="Settings__Content">
-          <ChangePassword />
+      <div id="Settings__Content">
+        <ChangePassword />
 
-          <ChangeTheme
-            updateTheme={this.props.updateTheme}
-            theme={this.props.theme}
-          />
-        </div>
-      </SettingsStyled>
-    );
-  }
-}
+        <ChangeTheme updateTheme={props.updateTheme} theme={props.theme} />
+      </div>
+    </SettingsStyled>
+  );
+};
 
 Settings.propTypes = {
   updateTheme: PropTypes.func,
